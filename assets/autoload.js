2c01f8a1a9c640b507e4a7b582ccd0fae9a0a3f6
yqmaster <61681161+yqmaster@users.noreply.github.com>
2021-02-24 20:02:06 +0800

    Add files via upload

--- assets/autoload.js
+++ assets/autoload.js
@@ -1,5 +1,4 @@
 try {
-    $("<link>").attr({href: "assets/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
     $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
     $.ajax({url: 'assets/waifu-tips.js?v=1.4.2',dataType:"script", cache: true, async: false});
     $.ajax({url: 'assets/live2d.js?v=1.0.5',dataType:"script", cache: true, async: false});
@@ -38,15 +37,15 @@ try {
     live2d_settings['showWelcomeMessage'] = true; // 显示进入面页欢迎词
 
     //看板娘样式设置
-    live2d_settings['waifuSize'] = '660x535'; // 看板娘大小，例如 '280x250', '600x535'
-    live2d_settings['waifuTipsSize'] = '570x150'; // 提示框大小，例如 '250x70', '570x150'
-    live2d_settings['waifuFontSize'] = '30px'; // 提示框字体，例如 '12px', '30px'
-    live2d_settings['waifuToolFont'] = '60px'; // 工具栏字体，例如 '14px', '36px'
-    live2d_settings['waifuToolLine'] = '100px'; // 工具栏行高，例如 '20px', '36px'
+    live2d_settings['waifuSize'] = '420x375'; // 看板娘大小，例如 '280x250', '600x535'
+    live2d_settings['waifuTipsSize'] = '375x105'; // 提示框大小，例如 '250x70', '570x150'
+    live2d_settings['waifuFontSize'] = '25px'; // 提示框字体，例如 '12px', '30px'
+    live2d_settings['waifuToolFont'] = '40px'; // 工具栏字体，例如 '14px', '36px'
+    live2d_settings['waifuToolLine'] = '80px'; // 工具栏行高，例如 '20px', '36px'
     live2d_settings['waifuToolTop'] = '-100px' // 工具栏顶部边距，例如 '0px', '-60px'
-    live2d_settings['waifuMinWidth'] = 'disable'; // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
-    live2d_settings['waifuEdgeSide'] = 'left:0'; // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
-    live2d_settings['waifuDraggable'] = 'unlimited'; // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
+    live2d_settings['waifuMinWidth'] = '1200px'; // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
+    live2d_settings['waifuEdgeSide'] = 'right:20'; // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
+    live2d_settings['waifuDraggable'] = 'disable'; // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
     live2d_settings['waifuDraggableRevert'] = false; // 松开鼠标还原拖拽位置，可选 true(真), false(假)
 
     // 其他杂项设置
